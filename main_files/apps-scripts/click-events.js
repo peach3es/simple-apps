d9666fa69e35436b4fe294527746beeadbc318ca
peaches <peizhe2001@hotmail.com>
2023-02-27 17:15:05 -0500

    Took off tilt.js no compatibility with mousedownevents

--- main_files/apps-scripts/click-events.js
+++ main_files/apps-scripts/click-events.js
@@ -1,3 +1,11 @@
+function enterCalc(){
+    let mouseOver = document.querySelector(".box");
+    // VanillaTilt.init(mouseOver, {
+    //     reverse: true,
+    //     glare: 0.3
+	// });
+}
+
 function openCalc(){
     console.log("calc opened!");
 
