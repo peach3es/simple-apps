de160e6a3d5c02436478905fd26c541bdc8fc005
peaches <peizhe2001@hotmail.com>
2023-03-03 14:57:56 -0500

    Set up animation of weather open & close

--- main_files/apps-scripts/click-events.js
+++ main_files/apps-scripts/click-events.js
@@ -21,8 +21,23 @@ function closeCalc(){
 
 
 function openWeather(){
-
+    const show = document.querySelector(".weather-modal");
+    const movedown = document.querySelector(".app-container");
+    show.classList.add("show");
+    show.classList.remove("remove");
+    movedown.classList.add("movedown");
+    movedown.classList.remove("moveup");
     console.log("weather opened!");
+}
+
+function closeWeather(){
+    // const remove = document.querySelector(".weather-modal");
+    // const moveup = document.querySelector(".app-container");
+    // remove.classList.remove("show");
+    // remove.classList.add("remove");
+    // moveup.classList.remove("movedown");
+    // moveup.classList.add("moveup");
+    console.log("weather closed!");
 }
 
 function openTodo(){
