5049a446b087229a25f6d2869bcb00cddf1d2355
peaches <peizhe2001@hotmail.com>
2023-07-22 23:55:58 -0400

    Weather app back end added air conditions with the API

--- apps-scripts/getWeather.js
+++ apps-scripts/getWeather.js
@@ -4,6 +4,10 @@ function getWeather() {
   let temp = document.getElementById("temp");
   let desc = document.getElementById("description");
   let location = document.getElementById("location");
+  let rf = document.getElementById("real-feel");
+  let wind = document.getElementById("wind");
+  let humidity = document.getElementById("humidity");
+  let pressure = document.getElementById("pressure");
 
   let api = "https://api.openweathermap.org/data/2.5/weather";
   let api2 = "https://api.openweathermap.org/data/2.5/forecast";
@@ -30,6 +34,11 @@ function getWeather() {
         // location.innerHTML = `${data.name} (${latitude}° ${longitude}°)`;
         location.innerHTML = `${data.name}`;
         desc.innerHTML = data.weather[0].description;
+        let real_feel = data.main.feels_like;
+        rf.innerHTML = `${real_feel.toFixed(0)}°C`;
+        humidity.innerHTML = data.main.humidity;
+        pressure.innerHTML = data.main.pressure;
+        wind.innerHTML = data.wind.speed;
       });
 
     fetch(apiURL2)
