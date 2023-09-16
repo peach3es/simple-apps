cc9ca22993685804ee7ad4c8129f0d6a65af6b2e
Peizhe Tian <peizhe2001@hotmail.com>
2023-09-16 14:52:10 -0400

    Change structure for API url

--- apps-scripts/getWeather.js
+++ apps-scripts/getWeather.js
@@ -1,4 +1,3 @@
-
 function getWeather() {
   let temp = document.getElementById("temp");
   let desc = document.getElementById("description");
@@ -8,50 +7,46 @@ function getWeather() {
   let humidity = document.getElementById("humidity");
   let pressure = document.getElementById("pressure");
 
-  let api = "https://api.openweathermap.org/data/2.5/weather";
-  let api2 = "https://api.openweathermap.org/data/2.5/forecast";
-  const apiKey = process.env.API_KEY;
-
   location.innerHTML = "Locating...";
 
   if ("geolocation" in navigator) {
+    let api = "https://api.openweathermap.org/data/2.5/weather";
+    let api2 = "https://api.openweathermap.org/data/2.5/forecast";
+    const apiKey = process.env.API_KEY;
 
-    navigator.geolocation.getCurrentPosition(
-      function success(position) {
-        let latitude = position.coords.latitude;
-        let longitude = position.coords.longitude;
-  
-        let apiURL1 = `${api}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-        let apiURL2 = `${api2}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-  
-        fetch(apiURL1)
-          .then((response) => response.json())
-          .then((data) => {
-            console.log(data);
-  
-            temp.innerHTML = `${data.main.temp.toFixed(0)}°C`;
-            // location.innerHTML = `${data.name} (${latitude}° ${longitude}°)`;
-            location.innerHTML = `${data.name}`;
-            desc.innerHTML = data.weather[0].description;
-  
-            rf.innerHTML = `${data.main.feels_like.toFixed(1)}°C`;
-            humidity.innerHTML = `${data.main.humidity.toFixed(0)}%`;
-            pressure.innerHTML = `${data.main.pressure.toFixed(1)} hPa`;
-            wind.innerHTML = `${data.wind.speed.toFixed(2)} km/h`;
-          });
-  
-        fetch(apiURL2)
-          .then((response) => response.json())
-          .then((data) => {
-            console.log(data);
-          });
-      }); 
-  } else{
+    navigator.geolocation.getCurrentPosition(function success(position) {
+      let latitude = position.coords.latitude;
+      let longitude = position.coords.longitude;
+
+      let apiURL1 = `${api}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+      let apiURL2 = `${api2}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+      fetch(apiURL1)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+
+          temp.innerHTML = `${data.main.temp.toFixed(0)}°C`;
+          // location.innerHTML = `${data.name} (${latitude}° ${longitude}°)`;
+          location.innerHTML = `${data.name}`;
+          desc.innerHTML = data.weather[0].description;
+
+          rf.innerHTML = `${data.main.feels_like.toFixed(1)}°C`;
+          humidity.innerHTML = `${data.main.humidity.toFixed(0)}%`;
+          pressure.innerHTML = `${data.main.pressure.toFixed(1)} hPa`;
+          wind.innerHTML = `${data.wind.speed.toFixed(2)} km/h`;
+        });
+
+      fetch(apiURL2)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        });
+    });
+  } else {
     location.innerHTML = "Unable to retrieve your location";
     console.error("Location was unable to be retrieved");
   }
-
-  
 }
 
 getWeather();
